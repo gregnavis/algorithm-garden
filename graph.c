28f9b79d68baa0737d3f08038360cd020dda268d
Grzegorz Niewisiewicz <grzegorz@niewisiewicz.pl>
2014-05-08 22:06:20 +0200

    Add a basic graph data structure
    The implementation using matrices with floating-point values isn't the
    most efficient one but it's definitely simple and allows me to reuse the
    matrix implementation that I'll use for other demonstrations too. In
    real life a matrix template (in C++) or matrix code generator (in C)
    would be a better idea.

---
+++ graph.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include "graph.h"
+
+struct graph *graph_new(size_t vertices)
+{
+	struct graph *G = malloc(sizeof(*G));
+	if (!G) {
+		return NULL;
+	}
+	G->adjecency = matrix_new(vertices, vertices);
+	return G;
+}
+
+void graph_free(struct graph *G)
+{
+	matrix_free(G->adjecency);
+	free(G);
+}
+
+void graph_connect(struct graph *G, size_t u, size_t v)
+{
+	matrix_set(G->adjecency, u, v, 1.0f);
+}
+
+void graph_disconnect(struct graph *G, size_t u, size_t v)
+{
+	matrix_set(G->adjecency, u, v, 0.0f);
+}
---
+++ graph.h
@@ -0,0 +1,11 @@
+#include "matrix.h"
+
+struct graph {
+	struct matrix *adjecency;
+};
+
+struct graph *graph_new(size_t vertices);
+void graph_free(struct graph *G);
+
+void graph_connect(struct graph *G, size_t u, size_t v);
+void graph_disconnect(struct graph *G, size_t u, size_t v);
